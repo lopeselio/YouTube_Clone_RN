fd9d5005e78af7df200899e8a1e1acdc6e3196bc
lopeselio <lopeselio@gmail.com>
2020-05-06 16:30:47 +0530

    adding color to header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import { View } from 'react-native'
+import { Entypo } from '@expo/vector-icons'
+import Constant from 'expo-constants'
 
 export default function Header () {
   return (
-    <View>
+    <View style={{
+      marginTop: Constant.statusBarHeight
+      height: 40,
+      backgroundColor: 'skyblue'
+    }}>
+      <View>
+        <Entypo name='youtube' color='red' size={28} />
+      </View>
+      <View>
       
+      </View>
     </View>
   )
 }
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { StyleSheet, Text, View } from 'react-native'
+import Header from '../components/Header'
 
 export default function HomeScreen () {
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <Header />
     </View>
   )
 }

17ea3938ea0a01cea721cf63793776a6f6f0a7ea
lopeselio <lopeselio@gmail.com>
2020-05-06 16:15:37 +0530

    first

--- App.js
+++ App.js
@@ -1,19 +1,11 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react'
+import { View } from 'react-native'
+import Home from './src/screens/Home'
 
-export default function App() {
+export default function App () {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{ flex: 1 }}>
+      <Home />
     </View>
-  );
+  )
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/screens/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function HomeScreen () {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  )
+}

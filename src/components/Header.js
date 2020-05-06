8e1850e3572e258c58fa325a8b883d4d6f9942f5
lopeselio <lopeselio@gmail.com>
2020-05-06 22:24:37 +0530

    added header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { View, Text } from 'react-native'
-import { Entypo } from '@expo/vector-icons'
+import { Entypo, Ionicons, MaterialIcons } from '@expo/vector-icons'
 import Constant from 'expo-constants'
 
 export default function Header () {
+  const myColor = '#212121'
   return (
     <View style={{
       marginTop: Constant.statusBarHeight,
       height: 40,
-      backgroundColor: 'skyblue'
+      backgroundColor: 'skyblue',
+      flexDirection: 'row',
+      justifyContent: 'space-between'
     }}
     >
       <View style={{
@@ -19,12 +22,23 @@ export default function Header () {
         <Entypo style={{ marginLeft: 20 }} name='youtube' color='red' size={32} />
         <Text style={{
           fontSize: 22,
-          marginLeft: 5
+          marginLeft: 5,
+          fontWeight: 'bold',
+          color: myColor
         }}
         > YouTube
         </Text>
       </View>
-      <View />
+      <View style={{
+        flexDirection: 'row',
+        marginRight: 20
+        // alignContent: 'flex-end'
+      }}
+      >
+        <Ionicons style={{ marginLeft: 5 }} name='md-videocam' size={32} color={myColor} />
+        <Ionicons style={{ marginLeft: 5 }} name='md-search' size={32} color={myColor} />
+        <MaterialIcons style={{ marginLeft: 5 }} name='account-circle' size={32} color={myColor} />
+      </View>
     </View>
   )
 }

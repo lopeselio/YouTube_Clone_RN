015c713161a36f48e4c83cb2b872c29a189afbe8
lopeselio <lopeselio@gmail.com>
2020-05-07 15:39:11 +0530

    card

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, Text, Image } from 'react-native'
-import { Entypo, Ionicons, MaterialIcons } from '@expo/vector-icons'
+import { MaterialIcons } from '@expo/vector-icons'
 
 const Card = () => {
   return (
@@ -9,10 +9,25 @@ const Card = () => {
         source={{ uri: 'https://unsplash.com/photos/fUnfEz3VLv4' }}
         style={{
           width: '100%',
-          height: 140
+          height: 200
         }}
       />
+      <View style={{
+        flexDirection: 'row',
+        margin: 5
+      }}
+      >
+        <MaterialIcons name='account-circle' size={32} color='#212121' />
+        <View style={{
+          marginLeft: 6
+        }}
+        >
+          <Text>This is a good Course</Text>
+          <Text>This is a good Course</Text>
+        </View>
+      </View>
     </View>
+
   )
 }
 

9781741d82b5ba6659c857025ec97d75a078893d
lopeselio <lopeselio@gmail.com>
2020-05-07 16:35:54 +0530

    created search screen component

---
+++ src/screens/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { View, Text, Image, Dimensions } from 'react-native'
+import { MaterialIcons } from '@expo/vector-icons'
+
+const searchScreen = () => {
+  return (
+    
+  )
+}
+
+export default SearchScreen

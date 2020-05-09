c80c6519b68dde6abf1c72dd6a6dbe257d443069
lopeselio <lopeselio@gmail.com>
2020-05-09 11:53:32 +0530

    Added packages and explore screen

---
+++ src/screens/Explore.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text, Image, Dimensions } from 'react-native'
+
+const Explore = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>I am Explore Screen</Text>
+    </View>
+
+  )
+}
+
+export default Explore

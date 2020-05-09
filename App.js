c13a44628f2a441d996f8eb61f1735d6f59c11f0
lopeselio <lopeselio@gmail.com>
2020-05-09 12:38:12 +0530

    react navigation imports

--- App.js
+++ App.js
@@ -2,6 +2,11 @@ import React from 'react'
 import { View } from 'react-native'
 // import HomeScreen from './src/screens/Home'
 import Constant from 'expo-constants'
+import { NavigationContainer } from '@react-navigation/native'
+import { createStackNavigator } from '@react-navigation/stack'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+
+
 
 // import Card from './src/components/Card'
 import SearchScreen from './src/screens/Search'

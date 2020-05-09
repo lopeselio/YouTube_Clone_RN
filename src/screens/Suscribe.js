697a6b3abc53d4b92abf14bb44b2856e94296171
lopeselio <lopeselio@gmail.com>
2020-05-09 12:13:22 +0530

    created subscribe and video files

---
+++ src/screens/Suscribe.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text, Image, Dimensions } from 'react-native'
+
+const Subscribe = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>I am Subscribe Screen</Text>
+    </View>
+
+  )
+}
+
+export default Subscribe
---
+++ src/screens/VidePlayer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { View, Text, Image, Dimensions } from 'react-native'
+
+const VideoPlayer = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text>I am Video Screen</Text>
+    </View>
+
+  )
+}
+
+export default VideoPlayer

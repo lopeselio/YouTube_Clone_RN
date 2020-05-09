359ebf816515c72d6d78436950fcc6fb4a1bd6f4
lopeselio <lopeselio@gmail.com>
2020-05-09 12:45:24 +0530

    added navigation container

--- App.js
+++ App.js
@@ -11,13 +11,8 @@ import SearchScreen from './src/screens/Search'
 
 export default function App () {
   return (
-    <View style={{
-      flex: 1,
-      marginTop: Constant.statusBarHeight
-    }}
-    >
-      {/* <HomeScreen /> */}
-      <SearchScreen />
-    </View>
+    <NavigationContainer>
+    
+    </NavigationContainer>
   )
 }

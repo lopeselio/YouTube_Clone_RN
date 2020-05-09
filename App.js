91652f8a2e5d8cbec5eb54047ca46b04b8204c9b
lopeselio <lopeselio@gmail.com>
2020-05-09 14:39:43 +0530

    created a rootHome component

--- App.js
+++ App.js
@@ -5,12 +5,26 @@ import Constant from 'expo-constants'
 import { NavigationContainer } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-
 // import Card from './src/components/Card'
 import SearchScreen from './src/screens/Search'
+import HomeScreen from './src/screens/Home'
+import Search from'./src/screens/Search'
+import VideoPlayer from './src/screens/VideoPlayer'
+import Explore from './src/screens/Explore'
+import Subscribe from './src/screens/Subscribe'
 
 const Stack = createStackNavigator()
 const Tabs = createBottomTabNavigator()
+
+const RootHome = () => {
+  return(
+    <Tabs.Navigator>
+      <Tabs.Screen name='home' component={HomeScreen} />
+      <Tabs.Screen name='explore' component={Explore} />
+      <Tabs.Screen name='home' component={Subscribe} />
+    </Tabs.Navigator>
+  )
+}
 
 export default function App () {
   return (

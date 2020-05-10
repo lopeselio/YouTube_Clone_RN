b0ff5f63a7461601e633f2f504be7529b30bc161
lopeselio <lopeselio@gmail.com>
2020-05-10 17:12:19 +0530

    explore screen

--- src/screens/Explore.js
+++ src/screens/Explore.js
@@ -2,11 +2,44 @@ import React from 'react'
 import { View, Text } from 'react-native'
 import Header from '../components/Header'
 
+const LittleCard = (props) => {
+  return (
+    <View style={{
+      backgroundColor: 'red',
+      height: 50,
+      width: 180,
+      borderRadius: 4,
+      marginTop: 10
+    }}
+    >
+      <Text style={{
+        textAlign: 'center',
+        color: 'white',
+        fontSize: 22,
+        marginTop: 5
+      }}
+      >Hello
+      </Text>
+    </View>
+  )
+}
 const Explore = () => {
   return (
     <View style={{ flex: 1 }}>
       <Header />
-      <Text>I am Explore Screen</Text>
+      <View style={{
+        flexDirection: 'row',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around'
+      }}
+      >
+        <LittleCard />
+        <LittleCard />
+        <LittleCard />
+        <LittleCard />
+        <LittleCard />
+        <LittleCard />
+      </View>
     </View>
 
   )

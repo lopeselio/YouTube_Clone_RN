5f36c9b8792ceb986299ef3a45a8ac7f570e9837
lopeselio <lopeselio@gmail.com>
2020-05-11 13:10:34 +0530

    fixed bugs

--- App.js
+++ App.js
@@ -1,112 +1,10 @@
-// import React from 'react'
-// import 'react-native-gesture-handler'
-// import { NavigationContainer, DefaultTheme, DarkTheme, useTheme } from '@react-navigation/native'
-// import { createStackNavigator } from '@react-navigation/stack'
-// import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
-// import { MaterialIcons } from '@expo/vector-icons'
-// import HomeScreen from './src/screens/Home'
-// import Search from './src/screens/Search'
-// import VideoPlayer from './src/screens/VideoPlayer'
-// import Explore from './src/screens/Explore'
-// import Subscribe from './src/screens/Subscribe'
-// import { reducer } from './src/reducers/reducer'
-// import { themeReducer } from './src/reducers/themeReducer'
-// import { Provider, useSelector } from 'react-redux'
-// import { createStore, combineReducers } from 'redux'
-
-// const customDarkTheme = {
-//   ...DarkTheme,
-//   colors: {
-//     ...DarkTheme.colors,
-//     headerColor: '#404040',
-//     iconColor: 'white',
-//     tabIcon: 'white'
-//   }
-// }
-
-// const customDefaultTheme = {
-//   ...DefaultTheme,
-//   colors: {
-//     ...DefaultTheme.colors,
-//     headerColor: 'white',
-//     iconColor: 'black',
-//     tabIcon: 'red'
-//   }
-// }
-// const rootReducer = combineReducers({
-//   cardData: reducer,
-//   myDarkMode: themeReducer
-// })
-
-// const store = createStore(rootReducer)
-
-// const Stack = createStackNavigator()
-// const Tabs = createBottomTabNavigator()
-
-// const RootHome = () => {
-//   const { colors } = useTheme()
-
-//   return (
-//     <Tabs.Navigator
-//       screenOptions={({ route }) => ({
-//         tabBarIcon: ({ color }) => {
-//           let iconName
-
-//           if (route.name === 'home') {
-//             iconName = 'home'
-//           } else if (route.name === 'explore') {
-//             iconName = 'explore'
-//           } else if (route.name === 'subscribe') {
-//             iconName = 'subscriptions'
-//           }
-
-//           // You can return any component that you like here!
-//           return <MaterialIcons name={iconName} size={25} color={color} />
-//         }
-//       })}
-//       tabBarOptions={{
-//         activeTintColor: colors.tabIcon,
-//         inactiveTintColor: 'gray'
-//       }}
-//     >
-//       <Tabs.Screen name='home' component={HomeScreen} />
-//       <Tabs.Screen name='explore' component={Explore} />
-//       <Tabs.Screen name='subscribe' component={Subscribe} />
-//     </Tabs.Navigator>
-//   )
-// }
-
-// // export default App = () => {
-// //   return (
-// //     <Provider store={store}>
-// //       <Navigation />
-// //     </Provider>
-// //   )
-// // }
-// export default function App () {
-//   const currentTheme = useSelector(state => {
-//     return state.myDarkMode
-//   })
-//   return (
-//     <Provider store={store}>
-//       <NavigationContainer theme={currentTheme ? customDarkTheme : customDefaultTheme}>
-//         <Stack.Navigator headerMode='none'>
-//           <Stack.Screen name='roothome' component={RootHome} />
-//           <Stack.Screen name='search' component={Search} />
-//           <Stack.Screen name='videoPlayer' component={VideoPlayer} />
-//         </Stack.Navigator>
-//       </NavigationContainer>
-//     </Provider>
-//   )
-// }
 import React from 'react'
-// import { StyleSheet, Text, View } from 'react-native'
+import 'react-native-gesture-handler'
 import { NavigationContainer, DefaultTheme, DarkTheme, useTheme } from '@react-navigation/native'
 import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
 import { MaterialIcons } from '@expo/vector-icons'
-// import Constant from 'expo-constants'
-import Home from './src/screens/Home'
+import HomeScreen from './src/screens/Home'
 import Search from './src/screens/Search'
 import VideoPlayer from './src/screens/VideoPlayer'
 import Explore from './src/screens/Explore'
@@ -135,18 +33,19 @@ const customDefaultTheme = {
     tabIcon: 'red'
   }
 }
-
-const rooReducer = combineReducers({
-  cardData: reducer, // [],
-  myDarkMode: themeReducer// false
+const rootReducer = combineReducers({
+  cardData: reducer,
+  myDarkMode: themeReducer
 })
-const store = createStore(rooReducer)
+
+const store = createStore(rootReducer)
 
 const Stack = createStackNavigator()
 const Tabs = createBottomTabNavigator()
 
 const RootHome = () => {
   const { colors } = useTheme()
+
   return (
     <Tabs.Navigator
       screenOptions={({ route }) => ({
@@ -162,7 +61,7 @@ const RootHome = () => {
           }
 
           // You can return any component that you like here!
-          return <MaterialIcons name={iconName} size={32} color={color} />
+          return <MaterialIcons name={iconName} size={25} color={color} />
         }
       })}
       tabBarOptions={{
@@ -170,18 +69,10 @@ const RootHome = () => {
         inactiveTintColor: 'gray'
       }}
     >
-      <Tabs.Screen name='home' component={Home} />
+      <Tabs.Screen name='home' component={HomeScreen} />
       <Tabs.Screen name='explore' component={Explore} />
       <Tabs.Screen name='subscribe' component={Subscribe} />
     </Tabs.Navigator>
-  )
-}
-
-export default function App () {
-  return (
-    <Provider store={store}>
-      <Navigation />
-    </Provider>
   )
 }
 
@@ -190,14 +81,22 @@ export function Navigation () {
     return state.myDarkMode
   })
   return (
+    <Provider store={store}>
+      <NavigationContainer theme={currentTheme ? customDarkTheme : customDefaultTheme}>
+        <Stack.Navigator headerMode='none'>
+          <Stack.Screen name='roothome' component={RootHome} />
+          <Stack.Screen name='search' component={Search} />
+          <Stack.Screen name='videoPlayer' component={VideoPlayer} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+  )
+}
 
-    <NavigationContainer theme={currentTheme ? customDarkTheme : customDefaultTheme}>
-      <Stack.Navigator headerMode='none'>
-        <Stack.Screen name='rootHome' component={RootHome} />
-        <Stack.Screen name='search' component={Search} />
-        <Stack.Screen name='videoplayer' component={VideoPlayer} />
-      </Stack.Navigator>
-    </NavigationContainer>
-
+export default function App () {
+  return (
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
   )
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,4 +1,4 @@
-// import React from 'react';
+import React from 'react'
 import { Text, View, Image, Dimensions, TouchableOpacity } from 'react-native'
 import { MaterialIcons } from '@expo/vector-icons'
 import { useNavigation, useTheme } from '@react-navigation/native'
